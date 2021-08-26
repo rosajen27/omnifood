6fff45d4ad6c93699a728cf652f8d171fd7aa6bf
Jennifer Rosa <rosajen27@gmail.com>
2021-08-26 10:07:30 -0400

    Dynamically update copyright year

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// Elements
+const yearEl = document.querySelector('.year');
+
+
+// Dynamically update copyright year
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
