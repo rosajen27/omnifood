623f2755a6b9125de5f262b04c71ecca44d1f9b6
Jennifer Rosa <rosajen27@gmail.com>
2021-08-30 10:30:29 -0400

    add mobile navigation functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,15 @@
-'use strict';
+"use strict";
 
 // Elements
-const yearEl = document.querySelector('.year');
-
+const yearEl = document.querySelector(".year");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
 // Dynamically update copyright year
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Mobile Navigation
+btnNavEl.addEventListener("click", function() {
+    headerEl.classList.toggle("nav-open");
+})
